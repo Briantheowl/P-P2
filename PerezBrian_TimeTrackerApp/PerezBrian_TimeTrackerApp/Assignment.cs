45578d750d2fbe98a9b6178a9d85919c4f2a752d
Brian Perez <BRPerez@student.fullsail.edu>
2020-02-14 23:44:34 -0500

    adding assignment folder to repo

---
+++ PerezBrian_TimeTrackerApp/PerezBrian_TimeTrackerApp/Assignment.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Data.Sql;
+
+namespace PerezBrian_TimeTrackerApp
+{
+    class Assignment
+    {
+        //Instantiating connection class
+        DBConn _connected = new DBConn();
+
+        //variables to hold the user's name and password
+        string _userName;
+        string _password;
+
+        public Assignment()
+        {
+            Console.WriteLine("Press any key to access login to your Time Tracker App");
+            Console.ReadKey();
+            
+            _userName = Validation.ValidateString("What is your First name?");
+
+            _password = Validation.ValidateString("What is your password?");
+            
+            //Running select statement through class object fields
+            _connected.Query(" SELECT user_firstname, user_password FROM time_tracker_users_05 " +
+            $" WHERE user_firstname = {_userName} AND user_password = {_password} ");
+
+            //Variable used to conatin all information of table
+            DataTable TempTable = _connected.QueryEx();
+
+            if (TempTable.Rows.Count != 0)
+            {
+                ItAllBeginsHere();
+            }
+            else
+            {
+                Console.WriteLine("Invalid login info, press any key to try again...");
+                Console.ReadKey();
+            }
+             
+
+        }
+
+        public void ItAllBeginsHere()
+        {
+            Console.WriteLine($"Hello {_userName}" + 
+                " What woud you like to do today?");
+        }
+
+    }
+}
---
+++ PerezBrian_TimeTrackerApp/PerezBrian_TimeTrackerApp/DBConn.cs
@@ -0,0 +1,45 @@
+﻿using System.Data;
+using MySql.Data.MySqlClient;
+
+
+
+namespace PerezBrian_TimeTrackerApp
+{
+    class DBConn
+    {
+        
+        //variable to hold connection string
+        private MySqlConnection _conn;
+        //variable used to activate the command line
+        public MySqlCommand _cmd;
+        //variable to fill and update datasets
+        private MySqlDataAdapter _da;
+        //variable to hold central object of database as a whole
+        private DataTable _dt;
+
+        public DBConn()
+        {
+            _conn = new MySqlConnection(@"server= 10.63.56.28.;username=brianPerez;password=root;database=BrianPerez_MDV229_Database_202002;port=8889");
+            _conn.Open();
+        } 
+
+        public void Query(string queryText)
+        {
+            _cmd = new MySqlCommand(queryText, _conn);
+        }
+
+        public DataTable QueryEx()
+        {
+            _da = new MySqlDataAdapter(_cmd);
+            _dt = new DataTable();
+            _da.Fill(_dt);
+            return _dt;
+        }
+
+        public void NonQueryEx()
+        {
+            _cmd.ExecuteNonQuery();
+        }
+       
+    }
+}
